6bfd5932c15911951db4fd22fd9ce79c92f40356
Abhinav K Subramanian <abhinavksubramanian@Abhinavs-MacBook-Air.local>
2018-07-11 14:38:50 +0530

    edited

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "user saved successfully" do
-    user = User.new(name: 'adasd', email: 'adada')
+    user = User.new(name: 'adasd', email: 'adada', address: 'adasdad')
     assert(user.save, "Saved successfully")
   end
 end
